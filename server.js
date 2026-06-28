2a0fc128aa06bd6c78322c7deaafa7c40cb31d53
Abigail <abby.w.gail@gmail.com>
2026-06-29 02:21:54 +0300

    cleanup4.1

--- server.js
+++ server.js
@@ -28,16 +28,24 @@ app.use(session({
 /* login lasts 365 days max */
 
 app.get("/", (req, res) => {
-	console.log("homepage requested");
 
-	res.sendFile(__dirname + "/public/index.html");
+	res.sendFile(
+		__dirname + "/public/index.html"
+	);
+
 });
 
 
 app.use(express.static("public"));
 
 
+app.get("/:username", (req, res) => {
 
+	res.sendFile(
+		__dirname + "/public/index.html"
+	);
+
+});
 
 const db = require("./database");
 
